e00cda605ebb307fd412143aca9d36ccc44564d0
Lukas Zumvorde <lzumvord@stackholmen.knut.univention.de>
2021-08-30 16:27:36 +0200

    fix(univention_config_registry): remove b in rstrip

--- plugins/modules/univention_config_registry.py
+++ plugins/modules/univention_config_registry.py
@@ -182,8 +182,8 @@ def _set_keys(keys, result, module):
     result['start']                = str(startd)
     result['end']                  = str(endd)
     result['delta']                = str(endd - startd)
-    result['out']                  = out.rstrip(b"\r\n")
-    result['err']                  = err.rstrip(b"\r\n")
+    result['out']                  = out.rstrip("\r\n")
+    result['err']                  = err.rstrip("\r\n")
     result['rc']                   = rc
     result['message']              = "These keys were set: {}".format(" ".join(to_set))
     result['meta']['changed_keys'] = to_set
